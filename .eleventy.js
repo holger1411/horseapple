d70296d4c2d775f30716f8cf66db87dadde8c707
Holger <holger1411@users.noreply.github.com>
2021-06-21 09:20:16 +0200

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,34 @@
+const {
+  DateTime
+} = require("luxon");
+
+module.exports = function(eleventyConfig) {
+  // Add a filter using the Config API
+  eleventyConfig.addWatchTarget("./src/scss/");
+  eleventyConfig.setBrowserSyncConfig({
+    reloadDelay: 400
+  });
+  eleventyConfig.addFilter("readableDate", dateObj => {
+    return DateTime.fromJSDate(dateObj, {
+      zone: 'utc'
+    }).toFormat("dd LLL yyyy");
+  });
+  // limit filter
+  eleventyConfig.addFilter("limit", function(array, limit) {
+    return array.slice(0, limit)
+    .reverse();
+  });
+  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+  eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+    return DateTime.fromJSDate(dateObj, {
+      zone: 'utc'
+    }).toFormat('yyyy-LL-dd');
+  });
+  return {
+    dir: {
+      input: "src",
+      output: "dev"
+    }
+  };
+
+};
